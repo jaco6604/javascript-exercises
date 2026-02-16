44fab1a749d9cdf91e59c048179c39c6c9ff1ec8
Ryan Jacobson <ryanjacobson1218@gmail.com>
2026-02-15 19:33:47 -0500

    Complete

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || typeof a !== 'number' || typeof b !== 'number') {
+        return "ERROR";
+    }
+    let sum = 0;
+    let start = (a < b) ? a : b;
+    let end = (a < b) ? b : a;
+    
+    for(i = start; i <= end; i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
