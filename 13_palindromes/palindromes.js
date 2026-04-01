228b0f5d977c856f4a89fbc9d7b05d37c19475cc
jaco6604 <ryanjacobson1218@gmail.com>
2026-04-01 17:48:06 -0400

    Complete

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+  for(let i = 0; i < cleanStr.length / 2; i++){
+    if(cleanStr[i] !== cleanStr[cleanStr.length - 1 - i]){
+        return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
