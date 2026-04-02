aa8177768519395f2f4a78f31e00e764f81c90c2
jaco6604 <ryanjacobson1218@gmail.com>
2026-04-01 21:50:19 -0400

    Complete

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    a = Number(a);
+    if(isNaN(a) || a < 0){
+        return "OOPS";
+    }
 
+    if (a === 0){
+        return 0;
+    }
+    if(a <= 2){
+        return 1;
+    }
+    let prev = 1;
+    let curr = 1;
+for(let i = 3; i <= a; i++){
+    let next = prev + curr;
+    prev = curr;
+    curr = next;
+    
+}
+
+return curr;
 };
 
 // Do not edit below this line
