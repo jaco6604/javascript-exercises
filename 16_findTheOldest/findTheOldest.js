d2494cebd963b12772cc2a6eaf4eb2799222c0d8
jaco6604 <ryanjacobson1218@gmail.com>
2026-04-02 20:53:19 -0400

    Complete

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    let oldest = people[0];
+    for(let i = 0; i < people.length; i++){
+        const person = people[i];
+        const death = person.yearOfDeath || new Date().getFullYear();
+        const oldestDeath = oldest.yearOfDeath || new Date().getFullYear();
+        const oldestLifeSpan = oldestDeath - oldest.yearOfBirth;
 
+        const lifespan = death - person.yearOfBirth;
+        if(lifespan > oldestLifeSpan){
+            oldest = person;
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
